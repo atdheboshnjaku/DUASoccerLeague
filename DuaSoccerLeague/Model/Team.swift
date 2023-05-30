4c76a6226103f0e69144ac5679e5458462e4232a
Atdhe Boshnjaku <atdhe.boshnjaku@gmail.com>
2023-05-30 14:18:11 +0200

    Create Team Class

--- DuaSoccerLeague/Model/Team.swift
+++ DuaSoccerLeague/Model/Team.swift
@@ -7,4 +7,26 @@
 
 import Foundation
 
-
+class Team {
+    
+    var id: Int
+    let name: String
+    var points: Int
+    var teamLogo: String
+    var coach: String
+    var stadiumName: String
+    let stadiumCapacity: Int
+    
+    init(id: Int, name: String, points: Int = 0, teamLogo: String, coach: String, stadiumName: String, stadiumCapacity: Int) {
+        
+        self.id = id
+        self.name = name
+        self.points = points
+        self.teamLogo = teamLogo
+        self.coach = coach
+        self.stadiumName = stadiumName
+        self.stadiumCapacity = stadiumCapacity
+        
+    }
+    
+}
